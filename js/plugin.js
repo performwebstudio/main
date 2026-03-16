8adc6b663501139b1a3c53fbce20825c4bc63faa
Shruti <srutisasmal02@gmail.com>
2026-03-16 18:49:16 +0530

    major changes related home page seo

---
+++ js/plugin.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // AOS
+  AOS.init({
+    duration: 900,
+    easing: "ease-out-cubic",
+    once: true,
+    mirror: false,
+    offset: 0,
+  });
+});
